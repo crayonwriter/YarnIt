32dfbf8c7c90593770f4d2832cdc4f0fa807627a
Theda Rogers <crayonwriter@gmail.com>
2020-11-12 02:02:35 -0500

    Update Instructions and YarnList fragments by connecting buttons. Remove duplicate actions in navigation.xml

--- app/src/main/java/com/crayonwriter/yarnit/InstructionsFragment.kt
+++ app/src/main/java/com/crayonwriter/yarnit/InstructionsFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.crayonwriter.yarnit.databinding.FragmentInstructionsBinding
 
 
@@ -18,6 +20,11 @@ private lateinit var binding: FragmentInstructionsBinding
     ): View? {
         // Inflate the layout for this fragment
        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
+
+       binding.listButton.setOnClickListener(
+               Navigation.createNavigateOnClickListener(R.id.action_fragment_instructions_to_yarnListFragment)
+       )
+
        return binding.root
     }
 }
--- app/src/main/java/com/crayonwriter/yarnit/YarnListFragment.kt
+++ app/src/main/java/com/crayonwriter/yarnit/YarnListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.crayonwriter.yarnit.databinding.FragmentYarnListBinding
 
 class YarnListFragment : Fragment() {
@@ -16,6 +17,11 @@ class YarnListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
             binding = DataBindingUtil.inflate(inflater, R.layout.fragment_yarn_list, container, false)
+
+            binding.floatingActionButton2.setOnClickListener(
+                    Navigation.createNavigateOnClickListener(R.id.action_yarnListFragment_to_addYarnDetailFragment)
+            )
+
             return binding.root
     }
 }
