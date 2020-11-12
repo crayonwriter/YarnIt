819ff67c9cbac7e28d6ceef6187660e76d6ef879
Theda Rogers <crayonwriter@gmail.com>
2020-11-11 19:58:17 -0500

    Update all layouts with <layout> tag

--- app/src/main/java/com/crayonwriter/yarnit/AddYarnDetailFragment.kt
+++ app/src/main/java/com/crayonwriter/yarnit/AddYarnDetailFragment.kt
@@ -5,15 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.crayonwriter.yarnit.databinding.FragmentAddYarnDetailBinding
 
 class AddYarnDetailFragment : Fragment() {
-    private lateinit var binding: AddYarnDetailFragmentBinding
+    private lateinit var binding: FragmentAddYarnDetailBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_yarn_detail, container, false)
+        //return inflater.inflate(R.layout.fragment_add_yarn_detail, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.id.addYarnDetailFragment, container, false)
+        return binding.root
     }
 }
