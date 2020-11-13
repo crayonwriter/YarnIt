600dad0ade8db2169b0e67924b7f3433e5f84b91
Theda Rogers <crayonwriter@gmail.com>
2020-11-12 20:26:41 -0500

    Add fun showYarnList() with sample list to YarnListViewModel and get reference in YarnListFragment. Connect Timber library to MainActivity.

--- app/src/main/java/com/crayonwriter/yarnit/YarnListFragment.kt
+++ app/src/main/java/com/crayonwriter/yarnit/YarnListFragment.kt
@@ -6,17 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.crayonwriter.yarnit.databinding.FragmentYarnListBinding
 
 class YarnListFragment : Fragment() {
     private lateinit var binding: FragmentYarnListBinding
+    private lateinit var viewModel: YarnListViewModel
+
         override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
             binding = DataBindingUtil.inflate(inflater, R.layout.fragment_yarn_list, container, false)
+
+
+        //Get a reference to the ViewModel by requesting it from the ViewModelProvider
+            viewModel =ViewModelProvider(this).get(YarnListViewModel::class.java)
 
             binding.floatingActionButton2.setOnClickListener(
                     Navigation.createNavigateOnClickListener(R.id.action_yarnListFragment_to_addYarnDetailFragment)
--- app/src/main/java/com/crayonwriter/yarnit/YarnListViewModel.kt
+++ app/src/main/java/com/crayonwriter/yarnit/YarnListViewModel.kt
@@ -1,11 +1,23 @@
 package com.crayonwriter.yarnit
 
 import androidx.lifecycle.ViewModel
+import timber.log.Timber
 
 class YarnListViewModel : ViewModel() {
-    private lateinit var yarnNameList : MutableList<String>
+    private lateinit var yarnNameList: MutableList<String>
 
-init {
+    init {
+        Timber.i("YarnListViewModel created!")
+        showYarnNameList()
+    }
 
-}
+
+    private fun showYarnNameList() {
+        yarnNameList = mutableListOf(
+                "Bernat Bulky",
+                "Red Heart Soft",
+                "Caron Super Soft"
+        )
+        yarnNameList.sort()
+    }
 }
