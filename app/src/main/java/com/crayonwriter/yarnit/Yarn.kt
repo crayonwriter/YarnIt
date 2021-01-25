043fa04229cd1761120c3dc833a794b17bf704d6
Theda Rogers <crayonwriter@gmail.com>
2021-01-25 04:42:54 -0500

    Create data class yarn and its variables/properties

---
+++ app/src/main/java/com/crayonwriter/yarnit/Yarn.kt
@@ -0,0 +1,8 @@
+package com.crayonwriter.yarnit
+
+data class Yarn (
+var yarnColorName: String = "",
+var yarnCompanyBrand: String = "",
+var yarnWeight: String = "",
+var yarnDescriptionType: String = ""
+)
