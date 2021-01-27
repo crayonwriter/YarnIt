1b30634bbae64579a09d58c9ab8abfab743e9625
Theda Rogers <crayonwriter@gmail.com>
2021-01-27 04:32:16 -0500

    Create binding connecting layout to class

--- app/src/main/java/com/crayonwriter/yarnit/YarnItem.kt
+++ app/src/main/java/com/crayonwriter/yarnit/YarnItem.kt
@@ -1,5 +1,28 @@
 package com.crayonwriter.yarnit
 
-class YarnItem {
+import android.content.Context
+import android.provider.ContactsContract
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.databinding.DataBindingUtil
+import com.crayonwriter.yarnit.databinding.YarnItemBinding
+
+class YarnItem(context: Context?) : LinearLayout(context) {
+
+    private var binding: YarnItemBinding = DataBindingUtil.inflate(LayoutInflater.from(context),
+    R.layout.yarn_item, this, false)
+
+fun addViewToList(yarn: YarnDataClass) {
+binding.apply {
+addView(this.root)
+yarnColorText.text = yarn.yarnColorName
+    yarnBrandText.text = yarn.yarnCompanyBrand
+    yarnWeightText.text = yarn.yarnWeight
+    yarnDescriptionText.text = yarn.yarnDescriptionType
+}
+}
+
 
 }
