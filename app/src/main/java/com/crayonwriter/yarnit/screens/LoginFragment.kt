2f13eab862e993caf3e16492b2f0222f0e86b8ac
Theda Rogers <crayonwriter@gmail.com>
2021-02-11 18:03:44 -0500

    Feat: Make Login screen the home screen and connect the buttons and navigation

--- app/src/main/java/com/crayonwriter/yarnit/screens/LoginFragment.kt
+++ app/src/main/java/com/crayonwriter/yarnit/screens/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.crayonwriter.yarnit.R
 import com.crayonwriter.yarnit.databinding.FragmentLoginBinding
 
@@ -16,6 +17,16 @@ class LoginFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        //Navigation - belongs in this UI Controller class
+        binding.loginExistingButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_welcomeFragment)
+        )
+
+        binding.addNewLoginButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_welcomeFragment)
+        )
+        
         return binding.root
     }
 
